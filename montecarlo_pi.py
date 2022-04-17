685c1134a0efbdb6c5dc64966883ae242e9736ae
Minimal_Engine <dawidrzeszutek@gmail.com>
2022-04-17 22:53:21 +0200

    montecarlo_pi.py fertiggestellt

--- montecarlo_pi.py
+++ montecarlo_pi.py
@@ -1,1 +1,14 @@
+import random
 
+
+print ("Script zur Berechnung von pi mit der Montecarlo-Methode")
+print ("Bitte geben Sie die Anzahl der Iterationen ein:")
+iterationen = int(input("Iterationen: "))
+nummern = 0
+
+for i in range(iterationen):
+    x = random.random()
+    y = random.random()
+    if x*x+y*y < 1:
+        nummern = nummern +1
+print ("Pi ist ungefähr", 4*nummern/iterationen)
