bbd291e064736d28ac7750009822cf39b0b6b135
Minimal-Engine <dawidrzeszutek@gmail.com>
2022-04-19 16:55:45 +0200

    Added self-reliance_regex.py
    Challenge with python and regular expressions

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Exercies
 Those are some small scripts, that I made to learn some python
-## montecarlo_pi
+## montecarlo_pi.py
 Script for calculating pi by using the monte carlo methode.
+## self-reliance_regex.py and emmerson_self-reliance.txt
+Solve the described problems using regular expressions and python.
---
+++ self-reliance_regex.py
@@ -0,0 +1,11 @@
+# Use the file emerson_self-reliance.txt and python with the re-module to solve the following questions:
+# original challenge see: https://www.linkedin.com/learning-login/share?account=2163266&forceAccount=false&redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Flearning-regular-expressions-2%2Fchallenge-characters%3Ftrk%3Dshare_video_url%26shareId%3DZg5DDeybTDaLUyC9kgb%252Feg%253D%253D
+# LinkedIn Learning Account required.
+
+# 1. how many times does the word "self" appear? (! case insensitive)
+
+# 2. count the words: himself,herself,itself,myself,yourself,thyself
+
+# 3. using three literal characters and three wildcards try matching the following words: please, palace, parade
+
+# 4. what matches /t..ch/ besides teach?
