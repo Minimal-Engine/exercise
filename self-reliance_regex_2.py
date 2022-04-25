09995875df417fabe1da2903774caf9161a91ea0
Minimal-Engine <dawidrzeszutek@gmail.com>
2022-04-25 17:03:48 +0200

    Added Exercise No. 2
    See LinkedIn-Learning- Course on regular expressions

---
+++ self-reliance_regex_2.py
@@ -0,0 +1,15 @@
+# Use the file emerson_self-reliance.txt and python with the re-module to solve the following questions:
+# original challenge see: https://www.linkedin.com/learning-login/share?account=2163266&forceAccount=false&redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Flearning-regular-expressions-2%2Fchallenge-character-sets%3Ftrk%3Dshare_video_url%26shareId%3DDTbnKbe7Rx6vSlSzZnHlWQ%253D%253D
+# LinkedIn Learning Account required.
+import re 
+import os
+
+fh =open('emerson_self-reliance.txt')
+os.system("cls")
+
+
+# Match both "lives" and "lived"
+# Match "virtue" but not "virtues"
+# Match the numbers and periods on all numbered paragraphs
+# Find the 16-Character word taht starts with "c"
+
