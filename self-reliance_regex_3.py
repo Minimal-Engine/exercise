7e5217c5aacc8994bbc56320fd5378bd7ff6bbf9
Minimal-Engine <dawidrzeszutek@gmail.com>
2022-05-09 13:14:19 +0200

    Update self-reliance_regex_3.py

---
+++ self-reliance_regex_3.py
@@ -0,0 +1,4 @@
+#match: self, himself, herself, itself, myself, yourself,thyself
+#match both: "virtue", and "virtues"
+#Use quantified Repetition to find the word that starts with "T" and has 12 letters
+#find all text that is inside quotation marks
